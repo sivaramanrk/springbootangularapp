acde00875cfe428f58f174aac3078668ca8797ac
unknown <sivaramanrk89@gmail.com>
2020-06-21 07:48:17 +0530

    second commit

---
+++ src/main/java/com/springboot/unittest/SpringJenkinsApplicationTest.java
@@ -0,0 +1,26 @@
+package com.springboot.unittest;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itextpdf.text.log.LoggerFactory;
+
+
+@RestController
+@RequestMapping("/app/test")
+public class SpringJenkinsApplicationTest {
+	
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringJenkinsApplicationTest.class);
+	
+	@GetMapping("/userlist")
+	@CrossOrigin(origins = "http://localhost:4200")
+	public String getAllCustomers() {
+		logger.info("test method is running...");
+		return "Hi Hello";
+	}
+}
